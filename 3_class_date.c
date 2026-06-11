21a0136024a882d6f6c2b8aef63f392536a40afd
Harshita <h9555841@gmail.com>
2026-06-11 12:07:46 +0530

    Create 3_class_date.c

---
+++ 3_class_date.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int date = 10;
+    int year = 2011;
+    int phone = 9968520231;
+    int class = 10;
+    int month = 6; 
+    
+    
+    printf("%d/0%d/%d\n",year,month,date);
+    printf("%d/0%d/%d\n",date,month,year);
+}
