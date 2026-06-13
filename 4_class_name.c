4f040ae2de9d83c37cdef0c3a4b43a121314cdf7
Harshita <h9555841@gmail.com>
2026-06-13 17:38:56 +0530

    Create 4_class_name.c

---
+++ 4_class_name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age = 16;
+    int player_score = 87;
+    
+    printf("student_age:%d\n",age);
+    printf("player's_score:%d\n", player_score);
+}
