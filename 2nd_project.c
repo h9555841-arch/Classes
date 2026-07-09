81fa84692b841b89444f9903904acb8b75d8fa7b
Harshita <h9555841@gmail.com>
2026-07-09 17:04:00 +0530

    Create 2nd_project.c

---
+++ 2nd_project.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void)
+{
+    long int harshu = 2021007487;
+    long int lucky = 8824541787;
+    long int farhan = 9821408821;
+    long int papa = 9968520231;
+    long int harshita = 9138132203;
+    
+    printf("-----------------------\n");
+    printf("| NAME    |  NUMBER   |\n");
+    printf("-----------------------\n");
+    printf("| harshu  | %li|\n", harshu);
+    printf("| lucky   | %li|\n", lucky);
+    printf("| farhan  | %li|\n",farhan);
+    printf("| papa    | %li|\n", papa);
+    printf("| harshita| %li|\n", harshita);
+    printf("-----------------------\n");
+}
